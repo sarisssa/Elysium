ba1e5004f038d724311a9b2f0dd33b7db888cd05
Michael Chan <mikechansas@gmail.com>
2021-12-22 18:46:55 -0800

    Create project with CRA and added homepage component

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>Accessories</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>Outerwear</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>Footwear</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>Lifestyle</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>Shop All</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
