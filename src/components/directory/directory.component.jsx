7fffb9d9662ea19473bb9bc084e394cd36b805df
Michael Chan <mikechansas@gmail.com>
2021-12-22 19:15:14 -0800

    Add Directory component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'accessories',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'shop/hats'
+                },
+                {
+                    title: 'outerwear',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                },
+                {
+                    title: 'footwear',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
+                },
+                {
+                    title: 'lifestyle',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
+                },
+                {
+                    title: 'shop all',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.sections.map(({ title, imageUrl, id, size }) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Directory;
