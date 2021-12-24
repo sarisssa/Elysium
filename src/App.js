b143d44523ad739544468e837311c10a23536983
Michael Chan <mikechansas@gmail.com>
2021-12-24 09:43:11 -0800

    Add Shop Page component with static data from shop.data.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const testPage = () => (
-  <div>
-    <h1>TESTING</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/test' component={testPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
