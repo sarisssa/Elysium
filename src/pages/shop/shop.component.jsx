15e8972d7c60a475989afc047b88da7ee450ed8a
Michael Chan <mikechansas@gmail.com>
2021-12-24 10:11:06 -0800

    Add Collection Preview component and integrate into Shop Page.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 import SHOP_DATA from './shop.data.js';
 
 class ShopPage extends React.Component {
@@ -12,9 +14,14 @@ class ShopPage extends React.Component {
     }
 
     render() {
+        const { collections } = this.state;
         return (
-            <div>SHOP PAGE</div>
-        )
+            <div className='shop-page'>
+                {collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} /> //Spread in title, routeName and items
+                ))}
+            </div>
+        );
     }
 }
 
