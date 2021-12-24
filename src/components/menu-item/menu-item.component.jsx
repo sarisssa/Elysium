26fd4c5fbea4bd3df0cb1e915e7e77c06b25144d
Michael Chan <mikechansas@gmail.com>
2021-12-23 19:20:08 -0800

    Add basic routing and refactor passing of props into Menu Item component.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from "react-router";
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className='content'>
             <div className='title'>{title.toUpperCase()}</div>
             <span className='subtitle'>SHOP NOW</span>
@@ -12,4 +13,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //Higher level component to pass match as props to Menu Item
+
