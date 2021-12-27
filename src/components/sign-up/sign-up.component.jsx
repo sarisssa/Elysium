e23e88e071e8444909f54146a240dc9c76a67965
Michael Chan <mikechansas@gmail.com>
2021-12-26 16:53:12 -0800

    Integrate Sign In component into Sign In and Sign Up Page.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -50,6 +50,12 @@ class SignUp extends React.Component {
         }
     };
 
+    handleChange = event => {
+        const { name, value } = event.target;
+    
+        this.setState({ [name]: value });
+      };
+
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
