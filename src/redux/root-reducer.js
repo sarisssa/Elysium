9dc0daf06df02489f61c13de8f9cba16e1dd648f
Michael Chan <mikechansas@gmail.com>
2021-12-28 14:58:42 -0800

    Amend Cart Icon component so that it subscribes to Redux store.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
-    user: userReducer //Each key equates to an individual slice of state
+    user: userReducer, //Each key equates to an individual slice of state
+    cart: cartReducer
 });
