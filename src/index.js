6a346b2d554bc51860b1fe8943a70c1c3a0df11d
Michael Chan <mikechansas@gmail.com>
2021-12-28 12:37:06 -0800

    Add Redux store and pass into Provider component.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import store from './redux/store';
 import App from './App';
 
+import './index.css';
 
 ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
