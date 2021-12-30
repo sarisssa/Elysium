6a8ba2181ee3a8df62504629715f64d267dda0a4
Michael Chan <mikechansas@gmail.com>
2021-12-30 13:41:26 -0800

    Connect Shop Page to Redux store. Note slight rename of selector names in shop selectors.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollections} from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
@@ -10,6 +13,8 @@ const ShopPage = ({ collections }) => (
     </div>
 );
 
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
