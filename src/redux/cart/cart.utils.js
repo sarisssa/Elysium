5f27e81e5eea7b2e5a2fb5176ed94fa7c3897409
Michael Chan <mikechansas@gmail.com>
2021-12-30 00:33:59 -0800

    Implement remove item from cart functionality within Redux cart profile.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,31 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-      cartItem => cartItem.id === cartItemToAdd.id //Assess if cartItem being added already exists within cart
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id //Assess if cartItem being added already exists within cart
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem => //Need to return new version of state to trigger re-render
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
-  
-    if (existingCartItem) {
-      return cartItems.map(cartItem => //Need to return new version of state to trigger re-render
-        cartItem.id === cartItemToAdd.id
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem
-      );
-    }
-  
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]; 
-  };
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) { //If cart item to be removed only has quantity of 1, remove it entirely from cart
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 } 
+      : cartItem
+  );
+};
