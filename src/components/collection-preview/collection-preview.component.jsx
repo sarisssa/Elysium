6ac15ba62d952c358e242a8c618126cc946a06f9
Michael Chan <mikechansas@gmail.com>
2021-12-29 17:42:47 -0800

    Amend Collection Preview and Collection Item component to integrate Add Item action.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, index) => index < 4)
-                .map(({ id, ...otherItemProps }) => ( //Limit displayed items to 4 under each category
-                    <CollectionItem key={id} {...otherItemProps} />
+                .map(item => ( //Limit displayed items to 4 under each category
+                    <CollectionItem key={item.id} item={item} />
                 ))}
         </div>
     </div>
