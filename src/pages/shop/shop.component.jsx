3c7588c9453bddfd5a8f56b5fea18b7ae261fd99
Michael Chan <mikechansas@gmail.com>
2021-12-30 13:33:40 -0800

    Begin refactor of Shop Page component from class to functional component.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,27 +2,14 @@ import React from 'react';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import SHOP_DATA from './shop.data.js';
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} /> //Spread in title, routeName and items 
+        ))}
+    </div>
+);
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} /> //Spread in title, routeName and items 
-                ))}
-            </div>
-        );
-    }
-}
 
 export default ShopPage;
