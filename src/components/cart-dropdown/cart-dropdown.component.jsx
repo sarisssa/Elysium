136e1ecf38e5bb498761d3c2e8f05927a04f9bca
Michael Chan <mikechansas@gmail.com>
2021-12-29 20:09:09 -0800

    Implement empty cart message in Cart Dropdown component with styling.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,14 +12,20 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            {cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))}
+            {cartItems.length ? (
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            ) : (
+                <span className='empty-message'>Your cart is empty</span>
+            )}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-const mapStateToProps =  createStructuredSelector({
+const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
-  });
+});
 
 export default connect(mapStateToProps)(CartDropdown);
