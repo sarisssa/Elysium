e5ca7a251d24b9f69e5f20802fc4ec28a0320e2c
Michael Chan <mikechansas@gmail.com>
2021-12-31 12:57:48 -0800

    Create new selector to pull data for  Collections Overview component.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,17 +2,14 @@ import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
-const COLLECTION_ID_MAP = {
-    accessories: 1,
-    sneakers: 2,
-    outerwear: 3,
-    womens: 4,
-    mens: 5
-};
-
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
 );
 
 export const selectCollection = collectionUrlParam =>
