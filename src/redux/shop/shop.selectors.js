604da97189e2952936878f81ebd9172cf58d8b3b
Michael Chan <mikechansas@gmail.com>
2021-12-31 12:36:50 -0800

    Modify COLLECTION_ID_MAP to point correctly to menu items in directory.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,9 +3,9 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 const COLLECTION_ID_MAP = {
-    hats: 1,
+    accessories: 1,
     sneakers: 2,
-    jackets: 3,
+    outerwear: 3,
     womens: 4,
     mens: 5
 };
