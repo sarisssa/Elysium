a6eb90e4cb7d0c6623e04b2f460c54459397fc4d
Michael Chan <mikechansas@gmail.com>
2021-12-31 12:29:26 -0800

    Integrate selectCollection selector into Collection Page.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,16 +3,27 @@ import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-    console.log(match.params.collectionId);
-
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
     return (
-    <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
-    </div>
-);}
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({ //Use second parameter ownProps to access all props of component
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
 
