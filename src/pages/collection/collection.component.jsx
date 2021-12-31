7c2d1b4ada0bf64ffc071d93af25fa5a99494cc4
Michael Chan <mikechansas@gmail.com>
2021-12-31 12:06:34 -0800

    Add minor changes to Collection Page.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const CollectionPage = () => (
+const CollectionPage = ({ match }) => {
+    console.log(match.params.collectionId);
+
+    return (
     <div className='collection-page'>
         <h2>COLLECTION PAGE</h2>
     </div>
-);
+);}
 
 export default CollectionPage;
 
