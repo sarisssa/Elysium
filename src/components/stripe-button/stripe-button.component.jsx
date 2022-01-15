8022a6e2eaeae33dad2262e3bb5bbbf298c7139b
Michael Chan <mikechansas@gmail.com>
2022-01-14 17:29:04 -0800

    Create stripe button component.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStrip = price * 100;
+    const publishableKey = 'pk_test_Vxiv5prmmrFF3JiM8hLtFcfi00iWXo8hou';
+
+    onToken = token => {
+        console.log(token);
+        alert('Success!')
+    } 
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='ELYSM Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStrip}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
