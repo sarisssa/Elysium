cc2506c77bbeb2b918111b9dfd0cfb1d037dc9a9
Michael Chan <mikechansas@gmail.com>
2022-01-15 15:04:34 -0800

    Refactor Custoom Button component to use styled components.

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,5 +1,23 @@
 import styled, { css } from "styled-components";
 
+const getButtonStyles = props => {
+    if (props.isGoogleSignIn) return googleSignInStyles;
+
+    return props.inverted ? invertedButtonStyles : buttonStyles;
+}
+
+const buttonStyles = css`
+    background-color: black;
+    color: white;
+    border: none;
+
+    &:hover {
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+      }
+`
+
 const invertedButtonStyles = css`
     background-color: white;
     color: black;
@@ -31,16 +49,10 @@ export const CustomButtonContainer = styled.button`
     line-height: 50px;
     padding: 0 35px 0 35px;
     font-size: 15px;
-    background-color: black;
-    color: white;
+    
     text-transform: uppercase;
     font-weight: bolder;
-    border: none;
     cursor: pointer;
 
-    &:hover {
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-      }
+    ${getButtonStyles}
 `;
