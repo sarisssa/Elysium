8f129891c322ad08dc6b42af0aa6629d1bfc30c6
Michael Chan <mikechansas@gmail.com>
2022-01-15 14:13:45 -0800

    Implement general tidy up of Header Comoponent and its styled component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils';
@@ -11,7 +10,12 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles.jsx';
+import { 
+    HeaderContainer, 
+    LogoContainer, 
+    OptionsContainer, 
+    OptionLink 
+} from './header.styles.jsx';
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -22,9 +26,9 @@ const Header = ({ currentUser, hidden }) => (
             <OptionLink to='/shop'>SHOP</OptionLink>
             <OptionLink to='/shop'>CONTACT</OptionLink> 
             {currentUser ? (
-                <OptionDiv onClick={() => auth.signOut()}>
+                <OptionLink as='div' onClick={() => auth.signOut()}>
                     SIGN OUT
-                </OptionDiv>
+                </OptionLink>
             ) : (
                 <OptionLink to='/signin'>SIGN IN</OptionLink>
             )}
