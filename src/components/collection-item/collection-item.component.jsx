4d9439e4249cfabebf5b8af57ea3c093133cc385
Michael Chan <mikechansas@gmail.com>
2022-01-15 18:25:36 -0800

    Refactor Collection Item component to use styled components.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
+
+import { 
+    CollectionItemContainer, 
+    CollectionFooterContainer, 
+    AddButton, 
+    BackgroundImage, 
+    NameContainer, 
+    PriceContainer 
+} from './collection-item.styles.jsx';
 
 import './collection-item.styles.scss';
 
@@ -11,14 +18,16 @@ const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
 
     return (
-        <div className='collection-item'>
-            <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
-        </div>
+        <CollectionItemContainer>
+            <BackgroundImage style={{ backgroundImage: `url(${imageUrl})` }} />
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick={() => addItem(item)}> 
+                Add to cart 
+            </AddButton>
+        </CollectionItemContainer>
     );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className='content'>
             <div className='title'>{title.toUpperCase()}</div>
-            <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 );
