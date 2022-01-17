33bdb425af15cf6460572a1aa332a46a70a81349
Michael Chan <mikechansas@gmail.com>
2022-01-16 16:48:18 -0800

    Connect Shop Page component into Redux store and along with integration of updateCollections as dispatch.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { firestore, convertCollectionsSnapshot } from '../../firebase/firebase.utils';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
 
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
         collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshot(snapshot);
+            updateCollections(collectionsMap);
             console.log(collectionsMap);
         });
     }
@@ -30,4 +34,8 @@ class ShopPage extends React.Component {
     }
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+    updateCollections: (collectionsMap) => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
