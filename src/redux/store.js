2328760900fb05bf151272b7d2eb74343c7dd09c
Michael Chan <mikechansas@gmail.com>
2022-01-18 13:50:56 -0800

    Integrate Redux Thunk into Redux store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [thunk, logger];
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
