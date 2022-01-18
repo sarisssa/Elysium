79b2d4a0107c381a4e218d9a780e9e8257687035
Michael Chan <mikechansas@gmail.com>
2022-01-17 16:07:08 -0800

    Amend Collection Preview component to allow users to click title and be routed to each Collection Page.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { withRouter } from "react-router";
+
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title' 
+            onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}>{title.toUpperCase()}
+        </h1>
         <div className='preview'>
             {items
                 .filter((item, index) => index < 4)
@@ -17,4 +21,4 @@ const CollectionPreview = ({ title, items }) => (
     </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
