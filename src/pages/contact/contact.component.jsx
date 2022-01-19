e58434c9f1b4210b52b22cc0220d2d79fd4500b8
Michael Chan <mikechansas@gmail.com>
2022-01-19 15:13:40 -0800

    Implement further styling on contact page and contact form.

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -4,13 +4,17 @@ import './contact.styles.scss';
 
 const ContactPage = () => (
     <div className='contact-page'>
-        <h1 className='contact-page-title'>
-            CONTACT US
-        </h1>
-        <h2>
-            WEBSTORE CUSTOMER SERVICE: SHOP@ELYSM.COM
-        </h2>
-        <ContactForm />
+        <div className='contact-page-header'>
+            <h1 className='contact-page-title'>
+                CONTACT US
+            </h1>
+            <h2>
+                WEBSTORE CUSTOMER SERVICE: SHOP@ELYSM.COM
+            </h2>
+        </div>
+        <div className='contact-page-body'>
+            <ContactForm />
+        </div>
     </div>
 );
 
