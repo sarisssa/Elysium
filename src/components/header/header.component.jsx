6ac759c83854a4f13423a8195f559f6543673b34
Michael Chan <mikechansas@gmail.com>
2022-01-19 10:21:05 -0800

    Amend Header contact to link to contact page.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils';
@@ -24,7 +23,7 @@ const Header = ({ currentUser, hidden }) => (
         </LogoContainer>
         <OptionsContainer>
             <OptionLink to='/shop'>SHOP</OptionLink>
-            <OptionLink to='/shop'>CONTACT</OptionLink> 
+            <OptionLink to='/contact'>CONTACT</OptionLink> 
             {currentUser ? (
                 <OptionLink as='div' onClick={() => auth.signOut()}>
                     SIGN OUT
