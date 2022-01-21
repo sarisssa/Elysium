4e0f64c92ee69e1ced2d28812c141dd4317b1aa0
Michael Chan <mikechansas@gmail.com>
2022-01-20 20:18:53 -0800

    Refactor Shop Page from class to functional component with hooks.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
 import { connect } from 'react-redux';
@@ -8,15 +8,10 @@ import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
-
-  componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
-  }
-
-  render() {
-    const { match } = this.props;
+const ShopPage = ({ fetchCollectionsStartAsync, match }) => {
+  useEffect(() => {
+    fetchCollectionsStartAsync()
+  }, []);
 
     return (
       <div className='shop-page'>
@@ -32,7 +27,6 @@ class ShopPage extends React.Component {
       </div>
     );
   }
-}
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
