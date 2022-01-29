9cfd983a10816f7cc5924a2ebb1169ca4bcd29a6
Michael Chan <mikechansas@gmail.com>
2022-01-29 15:41:48 -0800

    Implement Search Bar Icon Component.

---
+++ src/components/searchbar-icon/searchbar-icon.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleSearchBarHidden } from '../../redux/searchbar/searchbar.actions';
+
+import { ReactComponent as SearchingIcon } from '../../assets/searching.svg';
+
+const SearchBarIcon = ({ toggleSearchBarHidden }) => (
+    <div className='searchbar-icon' onClick={toggleSearchBarHidden}>
+        <SearchingIcon className='searching-icon'/>
+    </div>
+);
+
+const mapDispatchToProps = dispatch => ({
+    toggleSearchBarHidden: () => dispatch(toggleSearchBarHidden())
+});
+
+export default connect(null, mapDispatchToProps)(SearchBarIcon);
