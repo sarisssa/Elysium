68ea7e8ae54cdc954a909fc81e151f531266a6a5
Michael Chan <mikechansas@gmail.com>
2022-01-29 16:32:48 -0800

    Integrate Search Bar component into Header.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,48 +1,63 @@
+import { useState } from 'react';
+
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils';
 
+import SearchBar from '../search-bar/search-bar.component';
 import SearchBarIcon from '../search-bar-icon/search-bar-icon.component';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+import { selectSearchBarHidden } from '../../redux/searchbar/searchbar.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/pillar.svg';
 
-import { 
-    HeaderContainer, 
-    LogoContainer, 
-    OptionsContainer, 
-    OptionLink 
+import {
+    HeaderContainer,
+    LogoContainer,
+    OptionsContainer,
+    OptionLink
 } from './header.styles.jsx';
 
-const Header = ({ currentUser, isCartHidden }) => (
-    <HeaderContainer>
-        <LogoContainer to="/">
-            <Logo className='logo' />
-        </LogoContainer>
-        <OptionsContainer>
-            <OptionLink to='/shop'>SHOP</OptionLink>
-            <OptionLink to='/contact'>CONTACT</OptionLink> 
-            {currentUser ? (
-                <OptionLink as='div' onClick={() => auth.signOut()}>
-                    SIGN OUT
-                </OptionLink>
-            ) : (
-                <OptionLink to='/signin'>SIGN IN</OptionLink>
-            )}
-            <SearchBarIcon />
-            <CartIcon />
-        </OptionsContainer>
-        {isCartHidden ? null : <CartDropdown />}
-    </HeaderContainer>
-);
+const Header = ({ currentUser, isCartHidden, isSearchBarHidden }) => {
+
+    const [searchbar, setSearchbar] = useState('');
+
+    const onInputChange = event => {
+        setSearchbar(event.target.value)
+    }
+
+    return (
+        <HeaderContainer>
+            <LogoContainer to="/">
+                <Logo className='logo' />
+            </LogoContainer>
+            <OptionsContainer>
+                {isSearchBarHidden ? null : <SearchBar inputChange={onInputChange} />}
+                <OptionLink to='/shop'>SHOP</OptionLink>
+                <OptionLink to='/contact'>CONTACT</OptionLink>
+                {currentUser ? (
+                    <OptionLink as='div' onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </OptionLink>
+                ) : (
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
+                )}
+                <SearchBarIcon />
+                <CartIcon />
+            </OptionsContainer>
+            {isCartHidden ? null : <CartDropdown />}
+        </HeaderContainer>
+    );
+}
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
-    isCartHidden: selectCartHidden
+    isCartHidden: selectCartHidden,
+    isSearchBarHidden: selectSearchBarHidden
 });
 
 export default connect(mapStateToProps)(Header);
