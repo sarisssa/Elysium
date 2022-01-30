cc192998ed51ba6fa8f80b1e3324050434242505
Michael Chan <mikechansas@gmail.com>
2022-01-29 16:02:21 -0800

    Rename file path for search bar and properly integrate styling sheet.

--- src/components/search-bar-icon/search-bar-icon.component.jsx
+++ src/components/search-bar-icon/search-bar-icon.component.jsx
@@ -5,6 +5,8 @@ import { toggleSearchBarHidden } from '../../redux/searchbar/searchbar.actions';
 
 import { ReactComponent as SearchingIcon } from '../../assets/searching.svg';
 
+import './search-bar-icon.styles.scss';
+
 const SearchBarIcon = ({ toggleSearchBarHidden }) => (
     <div className='searchbar-icon' onClick={toggleSearchBarHidden}>
         <SearchingIcon className='searching-icon'/>
