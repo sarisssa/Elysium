f995423362bebb1da90a8db461ef6cd4617dac88
Michael Chan <mikechansas@gmail.com>
2022-02-01 15:01:03 -0800

    Implement search and match function within Search Bar component.

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,4 +1,27 @@
-const SearchBar = ({ searchfield, inputChange }) => {
+import { useState } from 'react';
+
+const SearchBar = ({ collections }) => {
+
+  const [searchbar, setSearchbar] = useState('');
+  const [products, setProducts] = useState([]);
+
+  //New searchBarDropdown component : pass products as prop
+  //If the length of products is > 0, then dynamically renders a div and map through products
+  //Else nothing and empty div
+
+  const search = (event) => {
+    setSearchbar(event.target.value);
+    const filteredProducts = match(event.target.value);
+    console.log('TEST TEST TEST', filteredProducts);
+    setProducts(filteredProducts);
+  }
+
+  const match = (s) => {
+    let values = collections.accessories.items;
+    const p = Array.from(s).reduce((a, v, i) => `${a}[^${s.substr(i)}]*?${v}`, '').toLowerCase();
+    const re = RegExp(p);
+    return values.filter(v => v.name.toLowerCase().match(re));
+  };
 
   return (
     <div className='search-bar'>
@@ -6,10 +29,13 @@ const SearchBar = ({ searchfield, inputChange }) => {
         className='search-bar-input'
         type='search'
         placeholder='Search'
-        onChange={inputChange}
+        value={searchbar}
+        onChange={event => search(event)}
       />
+      {/* <NEW COMPONENT HERE></NEW> */}
     </div>
   );
 }
 
 export default SearchBar;
+
