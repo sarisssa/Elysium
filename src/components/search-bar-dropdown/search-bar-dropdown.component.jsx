8fb3349ea8f9501d7b9e7231fb90e69a2e7373e8
Michael Chan <mikechansas@gmail.com>
2022-02-02 11:42:55 -0800

    Implement Search Bar Dropdown component.

---
+++ src/components/search-bar-dropdown/search-bar-dropdown.component.jsx
@@ -0,0 +1,11 @@
+const SearchBarDropdown = (props) => {
+    
+    const { products } = props;
+    console.log('PRODUCTS', products);
+
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default SearchBarDropdown;
