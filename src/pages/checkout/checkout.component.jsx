9b3ab985429b14f1730d71d31eab84a45a28e95a
Michael Chan <mikechansas@gmail.com>
2022-02-03 16:59:03 -0800

    Remove test warning from checkout page.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,11 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className='total'>TOTAL: ${total}</div>
-        <div className='test-warning'>
-            Please use the following test credit card for payments
-            <br />
-            4242 4242 4242 4242 - Exp: 01/23 - CVV: 268
-        </div>
+        
         <StripeCheckoutButton price={total} />
     </div>
 );
