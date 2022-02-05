878793384874b686f1121b2d936bb48e8578262b
Michael Chan <mikechansas@gmail.com>
2022-02-05 12:52:43 -0800

    Amend Collection Item SCSS for better presentation.

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -11,11 +11,11 @@ export const CollectionItemContainer = styled.div`
     
     &:hover {
         .image {
-        opacity: 0.8;
+            opacity: 0.8;
         }
         button {
-        opacity: 0.85;
-        display: flex;
+            opacity: 0.85;
+            display: flex;
         }
     }
 `;
@@ -23,11 +23,12 @@ export const CollectionItemContainer = styled.div`
 export const BackgroundImage = styled.div`
     width: 100%;
     height: 95%;
-    background-size: cover;
+    background-size: contain;
     background-position: center;
+    background-repeat: no-repeat;
     margin-bottom: 5px;
     background-image: ${({ imageUrl }) => `url(${imageUrl})`};
-    cursor: pointer
+    cursor: pointer;
 `;
 
 export const CollectionFooterContainer = styled.div`
@@ -39,13 +40,11 @@ export const CollectionFooterContainer = styled.div`
 `;
 
 export const NameContainer = styled.span`
-    width: 90%;
     margin-bottom: 15px;
 `;
 
 export const PriceContainer = styled.span`
-    width: 10%;
-    text-align: right;
+
 `;
 
 export const AddButton = styled(CustomButton)`
