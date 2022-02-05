56e9b79516c021e47a9848be47e3956e0d53fca0
Michael Chan <mikechansas@gmail.com>
2022-02-05 13:09:18 -0800

    Integrate Overlay, Search Bar Cancel Icon into Search Bar Component while adding styling.

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -6,7 +6,9 @@ import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import { sortProducts } from '../../utils/utils.js';
 
+import SearchBarCancelIcon from '../search-bar-cancel-icon/search-bar-cancel-icon.component';
 import SearchBarDropdown from '../search-bar-dropdown/search-bar-dropdown.component';
+import { Overlay } from '../header/header.styles.jsx';
 
 const SearchBar = ({ collections }) => {
 
@@ -21,17 +23,42 @@ const SearchBar = ({ collections }) => {
     setProducts(sortedProducts);
   }
 
+  const styles = {
+    position: 'fixed',
+    background: 'white',
+    top: '0',
+    left: '0',
+    display: 'flex',
+    padding: '2rem',
+    zIndex: 5,
+    width: '100%',
+    justifyContent: 'center'
+  };
+
+  const searchBarStyles = {
+    background: 'white',
+    borderRadius: '30px',
+    padding: '1rem 2rem',
+    maxWidth: '500px',
+    width: '100%',
+  };
+
   return (
-    <div className='search-bar'>
-      <input
-        className='search-bar-input'
-        type='search'
-        placeholder='Search'
-        value={searchbar}
-        onChange={event => search(event)}
-      />
-      {searchbar && searchbar.length > 0 ? <SearchBarDropdown products={products} /> : null}
-    </div>
+    <>
+      <div className='search-bar' style={styles}>
+        <input
+          className='search-bar-input'
+          type='search'
+          placeholder='Search'
+          value={searchbar}
+          onChange={event => search(event)}
+          style={searchBarStyles}
+        />
+        <SearchBarCancelIcon />
+        {searchbar && searchbar.length > 0 ? <SearchBarDropdown products={products} /> : null}
+      </div>
+      <Overlay />
+    </>
   );
 }
 
