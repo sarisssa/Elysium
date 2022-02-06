f24a78f60a54d1ed65813556f72e33c57ef91adb
Michael Chan <mikechansas@gmail.com>
2022-02-05 18:33:42 -0800

    Fix dependency array issue as warned by React.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = ({ fetchCollectionsStartAsync, setCurrentUser, currentUser, isSearch
 
   useEffect(() => {
     fetchCollectionsStartAsync()
-  }, []);
+  }, [fetchCollectionsStartAsync]);
 
   useEffect (() => {
     auth.onAuthStateChanged(async userAuth => {
