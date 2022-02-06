14d9f899bb3b359f79d51b2dd58981d917784893
Michael Chan <mikechansas@gmail.com>
2022-02-05 17:24:22 -0800

    Amend styling for Search Bar Item Component.

--- src/components/search-bar-item/search-bar-item.jsx
+++ src/components/search-bar-item/search-bar-item.jsx
@@ -1,17 +1,30 @@
+
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+
+import { toggleSearchBarHidden } from '../../redux/searchbar/searchbar.actions';
 
 import './search-bar-item.styles.scss';
 
-const SearchBarItem = ({ item, history }) => {
-    const { id, imageUrl, name, category} = item;
+const SearchBarItem = ({ item, history, toggleSearchBarHidden }) => {
+    const { id, imageUrl, name, category } = item;
 
     return (
-    <div className='search-bar-item' key={id} onClick={() => history.push(`/shop/${category.toLowerCase()}/${item.id}/details`)}>
-        <img src={imageUrl} alt='item'/>
-        <div className='item-details'>
-            <span className='name'>{name}</span>
+        <div className='search-bar-item' key={id} onClick={() => {
+            history.push(`/shop/${category.toLowerCase()}/${item.id}/details`)
+            toggleSearchBarHidden()
+        }}>
+            <img src={imageUrl} alt='item' />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+            </div>
         </div>
-    </div>
-);}
+    );
+}
 
-export default withRouter(SearchBarItem);
+const mapDispatchToProps = dispatch => ({
+    toggleSearchBarHidden: () => dispatch(toggleSearchBarHidden())
+});
+
+export default connect(null, mapDispatchToProps)(withRouter(SearchBarItem));
+
